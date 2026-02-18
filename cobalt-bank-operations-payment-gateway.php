280deb2f886ad00c1dcdee0ffa03efada617ff22
ander7agar <ander7agar@gmail.com>
2026-02-18 09:48:19 -0500

    Version v2.5.2

--- cobalt-bank-operations-payment-gateway.php
+++ cobalt-bank-operations-payment-gateway.php
@@ -5,7 +5,7 @@
  * Description: Payments with VISA, MasterCard and Clave
  * Author: Cobalt Tech
  * Author URI: https://neopayment.com
- * Version: 2.5.1
+ * Version: 2.5.2
  * License: GPL-2.0
  * Text Domain: cobalt-bank-operations-payment-gateway
  * Domain Path: /i18n
