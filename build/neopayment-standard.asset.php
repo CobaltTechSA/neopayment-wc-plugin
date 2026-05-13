84855207730f3af52411ba43e33fbc8a932b8814
ander7agar <ander7agar@gmail.com>
2026-05-12 19:44:45 -0500

    [CHANGE] Update payment gateway scripts and dependencies versions

--- build/neopayment-standard.asset.php
+++ build/neopayment-standard.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wp-i18n'), 'version' => 'e17bda84806a9639c0bc');
+<?php return array('dependencies' => array('react', 'wc-blocks-registry', 'wp-i18n'), 'version' => '940a4b975a400cae3b69');
